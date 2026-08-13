873dab63c827c6c0d182904f1d3adee653762890
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-08-13 11:34:21 +0800

    feat: add endpoint for getting a user's meetings #20

--- server/src/routes/meetingRoutes.ts
+++ server/src/routes/meetingRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { create } from "../controllers/meetingController.js";
+import { create, getAll } from "../controllers/meetingController.js";
 import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post("/", authenticate, create);
+router.get("/", authenticate, getAll);
 
 export default router;
