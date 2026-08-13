b579a8d73882c855e7577167348fa211eef7aec0
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-08-13 11:48:42 +0800

    feat: add controller for getting meeting by its id #19

--- server/src/controllers/meetingController.ts
+++ server/src/controllers/meetingController.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 import { AuthRequest } from "../middleware/authMiddleware.js";
-import { createMeeting, getMeetings } from "../services/meetingService.js";
+import { createMeeting, getMeetingById, getMeetings } from "../services/meetingService.js";
 
 export async function create(
     req: AuthRequest,
@@ -50,6 +50,44 @@ export async function getAll(
 
         return res.status(200).json({
             meetings,
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Internal server error",
+        });
+    }
+}
+
+export async function getById(
+    req: AuthRequest,
+    res: Response
+) {
+    try {
+        if (!req.userId) {
+            return res.status(401).json({
+                message: "Authentication required",
+            });
+        }
+        const { id } = req.params;
+
+        if (typeof id !== "string") {
+            return res.status(400).json({
+                message: "Meeting ID is required",
+            });
+        }
+
+        const meeting = await getMeetingById(id, req.userId);
+
+        if (!meeting) {
+            return res.status(404).json({
+                message: "Meeting not found",
+            });
+        }
+
+        res.status(200).json({
+            meeting,
         });
     } catch (error) {
         console.error(error);
