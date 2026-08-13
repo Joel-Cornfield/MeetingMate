3dc64e31ed024e2f0639d5276e74fdf9d2b1dbe9
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-08-13 11:50:02 +0800

    #20

--- server/src/routes/meetingRoutes.ts
+++ server/src/routes/meetingRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { create, getAll } from "../controllers/meetingController.js";
+import { create, getAll, getById } from "../controllers/meetingController.js";
 import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post("/", authenticate, create);
 router.get("/", authenticate, getAll);
+router.post("/:id", authenticate, getById);
 
 export default router;
