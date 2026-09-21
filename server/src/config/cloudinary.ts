8ebd1420da64b30a426c9e27badc96d4d2f1784f
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-09-21 11:27:06 +0800

    refactor: remove console logging from Cloudinary configuration

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -1,17 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-
 import { env } from "../config/env.js";
-
-console.log("Cloudinary config:");
-console.log("Cloud name:", env.CLOUDINARY_CLOUD_NAME);
-console.log(
-    "API key:",
-    env.CLOUDINARY_API_KEY ? "Present" : "Missing"
-);
-console.log(
-    "API secret:",
-    env.CLOUDINARY_API_SECRET ? "Present" : "Missing"
-);
 
 cloudinary.config({
     cloud_name: env.CLOUDINARY_CLOUD_NAME,
